34d15c7f25a159eaaea972365aabcfdb65e19237
Ezequiel Giannoni <egiannoni@frba.utn.edu.ar>
2022-10-16 21:05:00 -0300

    Struct fecha y producto

--- Proyecto_GastosFijos.cpp
+++ Proyecto_GastosFijos.cpp
@@ -2,8 +2,32 @@
 
 using namespace std;
 
+struct Producto {
+	float precio;
+	string nombreProducto;
+	string categoria;
+	struct Fecha;
+};
+
+struct Fecha {
+	int dia;
+	int mes;
+	int anio;
+};
+
 int main() {
-	
+	float Precio;
+	string nombreProducto;
+	int Dia;
+	cout << "Ingrese precio del producto" << endl;
+	cin >> Precio;
+	cout << "Ingrese precio del producto" << endl;
+	cin >> nombreProducto;
+	Producto info;
+	Fecha fechaDeCompra;
+	info.precio = Precio;
+	info.nombreProducto = nombreProducto;
+	fechaDeCompra.dia = Dia;
 	string Saludo;
 	int booli = false;
 	
