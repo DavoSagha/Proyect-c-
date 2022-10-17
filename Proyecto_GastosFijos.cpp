f9d1d9014629e989dcf7d95eda7a23a303e0315b
DavoSagha <saghatelyandsm@gmail.com>
2022-10-16 22:26:11 -0300

    Void producto terminado.

--- Proyecto_GastosFijos.cpp
+++ Proyecto_GastosFijos.cpp
@@ -16,42 +16,39 @@ struct Fecha {
 	int anio;
 };
 
-Fecha fecha (int dia, int mes, int anio){
-	cout << dia << " | " << mes << " | " << anio;
+void fecha(Fecha f) {
+
+	cout << f.dia << " | " << f.mes << " | " << f.anio;
 }
 
 void producto() {
 	float precio;
 	string nombreProducto;
 	string categoria;
-	char stock[500+1];
+	int id;
 	int dia, mes, anio;
+	Fecha f;
 
-
-	cout << "Ingrese precio del producto" << endl;
-	cin >> precio;
 	cout << "Ingrese nombre del producto" << endl;
 	cin >> nombreProducto;
+	cout << "Ingrese precio del producto" << endl;
+	cin >> precio;
 	cout << "Ingrese categoría del producto" << endl;
 	cin >> categoria;
-	cout << "Ingrese Numero del producto" << endl;
-	cin >> stock;
-	cout << "Ingrese la fecha de registro del producto"<< endl;
-	cin >> dia;
-	cin >> mes;
-	cin >> anio;
-	fecha(dia, mes, anio);
+	cout << "Ingrese id del producto" << endl;
+	cin >> id;
+	cout << "Ingrese la fecha de registro del producto (dia, mes, anio.. en ese orden)" << endl;
+	cin >> f.dia;
+	cin >> f.mes;
+	cin >> f.anio;
+	cout << "Producto: " << nombreProducto << ", precio: " << precio << ", categoria: " << categoria << ", id: " << id << "\n Fecha de registro: " << endl;
+	fecha(f);
+	cout << "\n Registrado con exito." << endl;
 }
 
 int main() {
-	
+
 	producto();
 
-	/*Fecha fechaDeCompra;
-	fechaDeCompra.dia = dia;
-	fechaDeCompra.mes = mes;
-	fechaDeCompra.anio = anio;*/
-  
-	
 	return 0;
 }
